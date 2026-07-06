1ec801dab7bb4361803c29ac5ff27e2feb514e38
Taiki <taiki.takamatsu@nsw.co.jp>
2026-07-06 16:17:13 +0900

    index.htmlまで追加

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const Main = (<h1>Markdown Editor</h1>)
+
+const root = createRoot(
+  document.getElementById('app')!
+);
+
+root.render(Main);
+
